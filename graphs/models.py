c9cae2353b9d8f52311737a083474221d19d1fc1
dilwaria <dilwaria@gmail.com>
2017-04-09 11:46:35 +0530

    last commiting before server

--- graphs/models.py
+++ graphs/models.py
@@ -16,6 +16,7 @@ class book1(models.Model):
     value8 = models.IntegerField()
     value9 = models.IntegerField()
     value10 = models.IntegerField()
+    table_type = models.IntegerField()
     
 
 
@@ -30,7 +31,7 @@ class book2(models.Model):
     value8 =models.DecimalField(max_digits=7, decimal_places=7)
     value9 = models.DecimalField(max_digits=7, decimal_places=7)
     value10 = models.DecimalField(max_digits=7, decimal_places=7)
-    
+    table_type = models.IntegerField()
 
 class book3(models.Model):
     value1 = models.DecimalField(max_digits=7, decimal_places=7)
@@ -43,7 +44,7 @@ class book3(models.Model):
     value8 =models.DecimalField(max_digits=7, decimal_places=7)
     value9 = models.DecimalField(max_digits=7, decimal_places=7)
     value10 = models.DecimalField(max_digits=7, decimal_places=7)
-    
+    table_type = models.IntegerField()
 
 
 class book4(models.Model):
@@ -57,31 +58,31 @@ class book4(models.Model):
     value8 =models.DecimalField(max_digits=7, decimal_places=7)
     value9 = models.DecimalField(max_digits=7, decimal_places=7)
     value10 = models.DecimalField(max_digits=7, decimal_places=7)
-    
+    table_type = models.IntegerField()
 
 
 class book5(models.Model):
-    value1 = models.IntegerField()
-    value2 = models.IntegerField()
-    value3 = models.IntegerField()
-    value4 = models.IntegerField()
-    value5 = models.IntegerField()
-    value6 = models.IntegerField()
-    value7 = models.IntegerField()
-    value8 = models.IntegerField()
-    value9 = models.IntegerField()
-    value10 = models.IntegerField()
-
+    value1 = models.DecimalField(max_digits=7, decimal_places=7)
+    value2 = models.DecimalField(max_digits=7, decimal_places=7)
+    value3 = models.DecimalField(max_digits=7, decimal_places=7)
+    value4 = models.DecimalField(max_digits=7, decimal_places=7)
+    value5 = models.DecimalField(max_digits=7, decimal_places=7)
+    value6 = models.DecimalField(max_digits=7, decimal_places=7)
+    value7 = models.DecimalField(max_digits=7, decimal_places=7)
+    value8 =models.DecimalField(max_digits=7, decimal_places=7)
+    value9 = models.DecimalField(max_digits=7, decimal_places=7)
+    value10 = models.DecimalField(max_digits=7, decimal_places=7)
+    table_type = models.IntegerField()
 
 class book6(models.Model):
-    value1 = models.IntegerField()
-    value2 = models.IntegerField()
-    value3 = models.IntegerField()
-    value4 = models.IntegerField()
-    value5 = models.IntegerField()
-    value6 = models.IntegerField()
-    value7 = models.IntegerField()
-    value8 = models.IntegerField()
-    value9 = models.IntegerField()
-    value10 = models.IntegerField()
-    
+    value1 = models.DecimalField(max_digits=7, decimal_places=7)
+    value2 = models.DecimalField(max_digits=7, decimal_places=7)
+    value3 = models.DecimalField(max_digits=7, decimal_places=7)
+    value4 = models.DecimalField(max_digits=7, decimal_places=7)
+    value5 = models.DecimalField(max_digits=7, decimal_places=7)
+    value6 = models.DecimalField(max_digits=7, decimal_places=7)
+    value7 = models.DecimalField(max_digits=7, decimal_places=7)
+    value8 =models.DecimalField(max_digits=7, decimal_places=7)
+    value9 = models.DecimalField(max_digits=7, decimal_places=7)
+    value10 = models.DecimalField(max_digits=7, decimal_places=7)
+    table_type = models.IntegerField()
--- graphs/views.py
+++ graphs/views.py
@@ -8,7 +8,7 @@ from django.http import JsonResponse,HttpResponse
 import openpyxl
 from openpyxl import Workbook
 from openpyxl import load_workbook
-from graphs.models import book1, book2, book3, book4
+from graphs.models import book1, book2, book3, book4, book5 ,book6
 from django.db.models import Max,Min,Sum
 import numpy 
 from random import randint
@@ -21,12 +21,12 @@ from graphs.forms import PostInputFormBook1,PostInputFormBook2,PostInputFormBook
 def introduction(request,variable_id=1):
 	# openpyxl.load_workbook()							
 	workbook = load_workbook('Book1.xlsx')
-	ws1 = workbook.get_sheet_by_name("Sheet4")
+	ws1 = workbook.get_sheet_by_name("Sheet7")
 	# print workBookBySheet.cell(row=1, column=2).value 
 	# print workBookBySheet.cell(row=1, column=3).value
 	# print ws1.cell(row=2,column=2).value
 	# for i in range(2,5002):
-	# 	x = book4(value1=ws1.cell(row=i, column=2).value,value2=ws1.cell(row=i, column=3).value,value3=ws1.cell(row=i, column=4).value,value4=ws1.cell(row=i, column=5).value,value5=ws1.cell(row=i, column=6).value,value6=ws1.cell(row=i, column=7).value,value7=ws1.cell(row=i, column=8).value,value8=ws1.cell(row=i, column=9).value,value9=ws1.cell(row=i, column=10).value,value10=ws1.cell(row=i, column=11).value  )
+	# 	x = book6(value1=ws1.cell(row=i, column=2).value,value2=ws1.cell(row=i, column=3).value,value3=ws1.cell(row=i, column=4).value,value4=ws1.cell(row=i, column=5).value,value5=ws1.cell(row=i, column=6).value,value6=ws1.cell(row=i, column=7).value,value7=ws1.cell(row=i, column=8).value,value8=ws1.cell(row=i, column=9).value,value9=ws1.cell(row=i, column=10).value,value10=ws1.cell(row=i, column=11).value  )
 	# 	x.save()
 		
 	print variable_id
@@ -47,28 +47,37 @@ def introduction(request,variable_id=1):
 		book =book1
 		FORM = PostInputFormBook1()
 
-	firstObs = book.objects.first()
-	lastObs = book.objects.last()
-	MaxObs = book.objects.all().aggregate(Max('value1'),Max('value2'),Max('value3'),Max('value4'),Max('value5'),Max('value6'),Max('value7'),Max('value8'),Max('value9'),Max('value10'))
-	MinObs = book.objects.all().aggregate(Min('value1'),Min('value2'),Min('value3'),Min('value4'),Min('value5'),Min('value6'),Min('value7'),Min('value8'),Min('value9'),Min('value10'))
-	SumObs = book.objects.all().aggregate(Sum('value1'),Sum('value2'),Sum('value3'),Sum('value4'),Sum('value5'),Sum('value6'),Sum('value7'),Sum('value8'),Sum('value9'),Sum('value10'))
+	firstObs = book.objects.filter(table_type=variable_id).first()
+	# for i in firstObs:
+	# print firstObs
+
+	lastObs = book.objects.filter(table_type=variable_id).last()
+	# print lastObs
+	MaxObs = book.objects.filter(table_type=variable_id).aggregate(Max('value1'),Max('value2'),Max('value3'),Max('value4'),Max('value5'),Max('value6'),Max('value7'),Max('value8'),Max('value9'),Max('value10'))
+	MinObs = book.objects.filter(table_type=variable_id).aggregate(Min('value1'),Min('value2'),Min('value3'),Min('value4'),Min('value5'),Min('value6'),Min('value7'),Min('value8'),Min('value9'),Min('value10'))
+	SumObs = book.objects.filter(table_type=variable_id).aggregate(Sum('value1'),Sum('value2'),Sum('value3'),Sum('value4'),Sum('value5'),Sum('value6'),Sum('value7'),Sum('value8'),Sum('value9'),Sum('value10'))
 	
 	largests = []
 	smallests = []
 	for i in range(1,11):
 		target = "-value"+str(i)
 		targetdesc = "value"+str(i)
-		nlargest = book.objects.all().order_by(target)[0]
-		nsmallest = book.objects.all().order_by(targetdesc)[0]
+		nlargest = book.objects.filter(table_type=variable_id).order_by(target)[0]
+		nsmallest = book.objects.filter(table_type=variable_id).order_by(targetdesc)[0]
 		largests.append(nlargest)
 		smallests.append(nsmallest)
 
 
-	for i in range(0,10):
-		# targetdesc = "value"+str(i)
-		print smallests[i].value1
+	# print smallests
+	for i in smallests:
+		print i.value1,i.value2,i.value3,i.value4,i.value5,i.value6,i.value7,i.value8,i.value9,i.value10
 
-	print smallests[0].value1
+
+	# for i in range(0,10):
+	# 	# targetdesc = "value"+str(i)
+	# 	print smallests[i].value1
+
+	# print smallests[0].value1
 	# nsmallest = 
 
 	finalarray =[]
@@ -114,7 +123,7 @@ def introduction(request,variable_id=1):
 					"variable":variable_id,
 					"largests": largests,
 					"smallests": smallests,}
-	return render(request, 'graphs/introduction.html', GET_CONTEXT)
+	return render(request, 'graphs/introductionagain.html', GET_CONTEXT)
 
 
 
@@ -224,47 +233,4 @@ def graphing(request,variable_id=1):
 	# GET_CONTEXT = {}
 	return render(request, 'graphs/graphing.html', GET_CONTEXT)
 
-
-def graphing2(request):
-	onlineorder=1
-	finedine=2
-	employed =3
-	self_employed =4
-	student =5
-	lessthan3lakh = 6
-	threetoseven = 7
-	sevento15 =8
-	fifteenplus =9 
-	outside =10
-	inside =11
-	daily=12
-	weekly=13
-	monthly=14
-	twiceinmonth = 15
-	withfriends =16
-	withfamily =17
-	alone =18
-	online =41
-	dinning = 42
-	lessthan30, thirtytosixty,moresixty = 43,44,45
-
-	for i in range(0,223):
-		time = randint(43,45)
-		print time
-
-
-
-	typer = randint(1,2)
-	challenge = randint(1,2)
-	occupation = randint(3,5)
-	anuallincome = randint(6,9)
-	eatoutside = randint(10,11)
-	frequency =  randint(12,15)
-	kskesaathh =  randint(16,18)
-	mode = randint(41,42)
-	time = randint(43,45)
-
-
-	GET_CONTEXT = {}
-	return render(request, 'graphs/graphing.html', GET_CONTEXT)
 
